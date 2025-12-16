3d0857f56d5c4460b5d198d0aaa9e47483cd1065
Mariya <Mariya@>
2025-12-16 19:29:47 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.GetsMA.Sprint7.Project.V4.Lib/BookService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Tyuiu.GetsMA.Sprint7.Project.V4.Lib
+{
+    public class BookService
+    {
+        public List<Book> LoadFromCsv(string path)
+        {
+            var books = new List<Book>();
+
+            foreach (var line in File.ReadAllLines(path))
+            {
+                var parts = line.Split(';');
+                books.Add(new Book
+                {
+                    Author = parts[0],
+                    Title = parts[1],
+                    Year = int.Parse(parts[2]),
+                    Price = decimal.Parse(parts[3]),
+                    IsNew = bool.Parse(parts[4])
+                });
+            }
+            return books;
+        }
+
+        public void SaveToCsv(string path, List<Book> books)
+        {
+            var lines = books.Select(b =>
+                $"{b.Author};{b.Title};{b.Year};{b.Price};{b.IsNew}");
+            File.WriteAllLines(path, lines);
+        }
+
+        public decimal GetAveragePrice(List<Book> books)
+        {
+            return books.Average(b => b.Price);
+        }
+    }
+}
+
---
+++ Tyuiu.GetsMA.Sprint7.Project.V4.Test/CsvServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.GetsMA.Sprint7.Project.V4.Lib.Services;
+
+namespace Tyuiu.GetsMA.Sprint7.Project.V4.Test
+{
+    [TestClass]
+    public class CsvServiceTest_GMA
+    {
+        [TestMethod]
+        public void SaveAndLoadBooks_ShouldWorkCorrectly()
+        {
+            CsvService_GMA service = new CsvService_GMA();
+            Assert.IsNotNull(service);
+        }
+    }
+}
